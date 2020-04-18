3f96a8f6790fbcbb57cd4032a2621b6b76e37dbc
Kaden Lovell <kadenlovell@live.com>
2020-04-18 02:57:19 -0600

    initial checkpoint with blabs barely working

---
+++ BlabberApp/BlabberApp.DataStore/Sql/blabs.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `kadenlovell`.`blabs`;
+SELECT * FROM `kadenlovell`.`users`;
+TRUNCATE `kadenlovell`.`blabs`;
+TRUNCATE `kadenlovell`.`users`;
---
+++ BlabberApp/BlabberApp.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,7 @@
+using System;
+using BlabberApp.Domain.Entities;
+
+namespace BlabberApp.Domain.Interfaces {
+    public interface IUserRepository : IRepository<User> {
+    }
+}
